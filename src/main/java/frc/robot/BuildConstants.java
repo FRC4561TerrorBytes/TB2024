ab6841413cbf75ef3d015b1141155278fb30cb42
Tyler Melton <ninjanarwhal07@gmail.com>
2024-01-11 18:04:00 -0500

    VisionSubsystem base created

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "cccf07d099af04529cc8548c1b752a398506537a";
-  public static final String GIT_DATE = "2024-01-09 21:33:45 EST";
-  public static final String GIT_BRANCH = "Add-git-hash-to-deploy";
-  public static final String BUILD_DATE = "2024-01-09 21:54:31 EST";
-  public static final long BUILD_UNIX_TIME = 1704855271542L;
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "ed1a7196f5bebe21b82bec29175eb3e0ed1ec7eb";
+  public static final String GIT_DATE = "2024-01-09 21:58:59 EST";
+  public static final String GIT_BRANCH = "VisionSubsystem";
+  public static final String BUILD_DATE = "2024-01-11 18:02:40 EST";
+  public static final long BUILD_UNIX_TIME = 1705014160150L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.LimelightResults;
+import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
+
+public class VisionSubsystem extends SubsystemBase {
+
+  Drive m_driveSubsytem;
+
+  public VisionSubsystem(Drive driveSubsystem) {
+    m_driveSubsytem = driveSubsystem;
+  }
+
+  private LimelightTarget_Fiducial getClosestTag(String cameraName) {
+    double closest = 100;
+    LimelightTarget_Fiducial target = null;
+    LimelightTarget_Fiducial[] targetList = LimelightHelpers.getLatestResults(cameraName).targetingResults.targets_Fiducials;
+    for (LimelightTarget_Fiducial i : targetList) {
+      double value = i.tx;
+      if (value < closest) {
+        closest = value;
+        target = i;
+      }
+    }
+    return target;
+  }
+
+  public LimelightTarget_Fiducial getTagByID(LimelightTarget_Fiducial[] targets, double ID) {
+    for (LimelightTarget_Fiducial t : targets) {
+      if (t.fiducialID == ID) {
+        return t;
+      }
+    }
+    return null;
+  }
+
+  public void updateOdometry() {
+    LimelightResults results = LimelightHelpers.getLatestResults("limelight-right");
+    if (getClosestTag("limelight-right") != null) {
+      //m_driveSubsytem.addVision(results);
+      Logger.recordOutput("updating with tags", true);
+    } else {
+      Logger.recordOutput("updating with tags", false);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    updateOdometry();
+  }
+}
