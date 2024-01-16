c803cf9f1cd3d164b15a1f0f7ebf2480c8c849d7
Tyler Melton <ninjanarwhal07@gmail.com>
2024-01-15 19:49:31 -0500

    more changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "f25de743cb92b3dd005ee1a5cb29966928cfea4f";
-  public static final String GIT_DATE = "2024-01-15 15:56:14 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "e356408dd13885f7e3d24ef4cbf6ec2e81c290f4";
+  public static final String GIT_DATE = "2024-01-15 19:10:57 EST";
   public static final String GIT_BRANCH = "Drivebase-akit";
-  public static final String BUILD_DATE = "2024-01-15 18:24:52 EST";
-  public static final long BUILD_UNIX_TIME = 1705361092667L;
+  public static final String BUILD_DATE = "2024-01-15 19:41:35 EST";
+  public static final long BUILD_UNIX_TIME = 1705365695237L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
   public static final int FRONT_LEFT_STEER_ENCODER = 23;
   public static final boolean FRONT_LEFT_DRIVE_MOTOR_INVERTED = true;
   public static final boolean FRONT_LEFT_TURN_MOTOR_INVERTED = true;
-  public static final double FRONT_LEFT_STEER_OFFSET = 0.4675292;
+  public static final double FRONT_LEFT_STEER_OFFSET = -0.4675292;
 
   public static final int FRONT_RIGHT_DRIVE_MOTOR = 3;
   public static final int FRONT_RIGHT_STEER_MOTOR = 4;
@@ -87,14 +87,14 @@ public final class Constants {
   public static final int BACK_LEFT_STEER_ENCODER = 24;
   public static final boolean BACK_LEFT_DRIVE_MOTOR_INVERTED = false;
   public static final boolean BACK_LEFT_TURN_MOTOR_INVERTED = true;
-  public static final double BACK_LEFT_STEER_OFFSET = 0.0771484375;
+  public static final double BACK_LEFT_STEER_OFFSET = -0.0771484375;
 
   public static final int BACK_RIGHT_DRIVE_MOTOR = 1;
   public static final int BACK_RIGHT_STEER_MOTOR = 2;
   public static final int BACK_RIGHT_STEER_ENCODER = 21;
   public static final boolean BACK_RIGHT_DRIVE_MOTOR_INVERTED = true;
   public static final boolean BACK_RIGHT_TURN_MOTOR_INVERTED = true;
-  public static final double BACK_RIGHT_STEER_OFFSET = 0.333251953;
+  public static final double BACK_RIGHT_STEER_OFFSET = -0.333251953;
 
   public static final double DRIVE_CURRENT_LIMIT = 80.0;
   public static final int TURN_CURRENT_LIMIT = 40;
