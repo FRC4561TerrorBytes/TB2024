2210eb300bd2bfd6b3e51362f4a6020692b00466
ethanprivette <dabosses444@gmail.com>
2024-01-19 16:39:02 -0500

    did a quick 3 piece middle auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 19;
-  public static final String GIT_SHA = "cd073382dffcc96ed24af2520ec3680aec6cee1c";
-  public static final String GIT_DATE = "2024-01-18 21:59:04 EST";
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "571f0876a37bd82c0a4973ccc77dc5bfe844d4da";
+  public static final String GIT_DATE = "2024-01-19 12:19:18 EST";
   public static final String GIT_BRANCH = "PP-sim-testing";
-  public static final String BUILD_DATE = "2024-01-19 11:53:17 EST";
-  public static final long BUILD_UNIX_TIME = 1705683197969L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-01-19 16:26:09 EST";
+  public static final long BUILD_UNIX_TIME = 1705699569022L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
