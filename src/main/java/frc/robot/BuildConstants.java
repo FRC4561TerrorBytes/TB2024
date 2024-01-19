cd073382dffcc96ed24af2520ec3680aec6cee1c
ethanprivette <112294818+ethanprivette@users.noreply.github.com>
2024-01-18 21:59:04 -0500

    I can't believe that worked first try this is so easy I love it

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "73c5a0ca0e5dc87fbbc217a33709e6a00d04c2b8";
-  public static final String GIT_DATE = "2024-01-18 18:06:18 EST";
-  public static final String GIT_BRANCH = "Drivebase-akit";
-  public static final String BUILD_DATE = "2024-01-18 18:44:53 EST";
-  public static final long BUILD_UNIX_TIME = 1705621493561L;
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "251eb602c3843c28896d8ef9687b2b4308ae9683";
+  public static final String GIT_DATE = "2024-01-18 18:59:12 EST";
+  public static final String GIT_BRANCH = "PP-sim-testing";
+  public static final String BUILD_DATE = "2024-01-18 21:36:24 EST";
+  public static final long BUILD_UNIX_TIME = 1705631784881L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
