c86fd950005c751d4c45815011af98a705779fa5
ethanprivette <dabosses444@gmail.com>
2024-01-20 11:38:23 -0500

    Autos and mirroring works ig

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "571f0876a37bd82c0a4973ccc77dc5bfe844d4da";
-  public static final String GIT_DATE = "2024-01-19 12:19:18 EST";
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "2210eb300bd2bfd6b3e51362f4a6020692b00466";
+  public static final String GIT_DATE = "2024-01-19 16:39:02 EST";
   public static final String GIT_BRANCH = "PP-sim-testing";
-  public static final String BUILD_DATE = "2024-01-19 16:26:09 EST";
-  public static final long BUILD_UNIX_TIME = 1705699569022L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-01-20 11:25:47 EST";
+  public static final long BUILD_UNIX_TIME = 1705767947290L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
