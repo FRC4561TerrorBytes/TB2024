e5343d0f924b2bb16f6688bac80e6835bae68b8b
Tyler Melton <ninjanarwhal07@gmail.com>
2024-01-22 17:58:08 -0500

    teleop drive fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "2210eb300bd2bfd6b3e51362f4a6020692b00466";
-  public static final String GIT_DATE = "2024-01-19 16:39:02 EST";
-  public static final String GIT_BRANCH = "PP-sim-testing";
-  public static final String BUILD_DATE = "2024-01-20 11:25:47 EST";
-  public static final long BUILD_UNIX_TIME = 1705767947290L;
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "3470d8d9a52c113a77bd02046f7f722f0701e178";
+  public static final String GIT_DATE = "2024-01-20 15:11:14 EST";
+  public static final String GIT_BRANCH = "Teleop-Fix";
+  public static final String BUILD_DATE = "2024-01-22 17:25:22 EST";
+  public static final long BUILD_UNIX_TIME = 1705962322088L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
