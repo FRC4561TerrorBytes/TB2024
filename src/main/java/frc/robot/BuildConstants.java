fea6ff18a7fceeaf779a83fd689b7aecce62f515
Tyler Melton <ninjanarwhal07@gmail.com>
2024-01-25 17:58:50 -0500

    Shooter profiling

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 42;
-  public static final String GIT_SHA = "8c73f82ba3402c637fbef4f3b512835fd8fac4c7";
-  public static final String GIT_DATE = "2024-01-22 19:12:01 EST";
+  public static final int GIT_REVISION = 43;
+  public static final String GIT_SHA = "1102c85e01399a80b8baeb2b4910b47ae14c3bd0";
+  public static final String GIT_DATE = "2024-01-22 19:36:59 EST";
   public static final String GIT_BRANCH = "IntakeTest";
-  public static final String BUILD_DATE = "2024-01-22 19:34:28 EST";
-  public static final long BUILD_UNIX_TIME = 1705970068916L;
+  public static final String BUILD_DATE = "2024-01-25 14:47:40 EST";
+  public static final long BUILD_UNIX_TIME = 1706212060395L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,11 +7,20 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
   private final CANSparkMax m_topRoller = new CANSparkMax(15, MotorType.kBrushless);
   private final CANSparkMax m_bottomRoller = new CANSparkMax(16, MotorType.kBrushless);
+
+  private static double distance = 4.5;
+  private static double velocity;
+  private static double angle = Units.degreesToRadians(54.8);
+  private static double height = 0.7;
+
+  private static double midpointY = 1.974;
+  private static double midpointX = 0.196;
 
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
@@ -31,5 +40,60 @@ public class IntakeSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
 
+  }
+  public static double findVelocity(double x){
+    return Math.sqrt((((
+                    (-1*(9.8*Math.pow(-x, 2)))
+              /((midpointY + height)/(-x*Math.tan(angle))))
+                /Math.pow(Math.cos(angle), 2)))
+                                /2);
+  }
+  public static double  findTrajectoryPoint(double x){
+    System.out.println("angle in trajectory: " + angle);
+    return((distance + x)*Math.tan(angle)-((9.8*Math.pow((distance + x), 2)) / (2*Math.pow(findVelocity(distance), 2) * Math.pow(Math.cos(angle), 2))));
+  }
+  public static double findBestAngle(){
+    double add = 0.1;
+    int searchLength = 100;
+    if(findTrajectoryPoint(-midpointX) < midpointX){
+      add *= -1;
+    }
+
+    double startAngle = Units.radiansToDegrees(Math.atan((midpointY-height)/(distance - midpointX)))+6;
+
+    System.out.println("add: " + add);
+    System.out.println("starting angle: " + startAngle);
+
+    double[] angles = new double[searchLength];
+
+    for(int i = 0; i < searchLength; i++){
+      angles[i] = startAngle+(add*i);
+    }
+
+    double closestError = 360;
+    int angleIndex = 0;
+    for(int i = 0; i < searchLength; i++){
+      angle = Units.degreesToRadians(angles[i]);
+      double error = Math.abs(findTrajectoryPoint(-midpointX) - (midpointY - height));
+      System.out.println("error: " + error + " angle: " + Units.radiansToDegrees(angle));
+      System.out.println("\n Trajectory Point: " + Math.abs(findTrajectoryPoint(-midpointX)) + "\n");
+      if (error < closestError){
+        closestError = error;
+        angleIndex = i;
+      }
+    }
+    return angles[angleIndex];
+  }
+  public static void main(String[] args){
+    System.out.println("test");
+    //System.out.println(findTrajectoryPoint(-midpointX));
+    //System.out.println(findVelocity(distance));
+    
+    double bestAngle = findBestAngle();
+    System.out.println("best angle: " + bestAngle);
+    
+    angle = Units.degreesToRadians(bestAngle);
+    
+    System.out.println("velocity m/s: " + findVelocity(distance));
   }
 }
