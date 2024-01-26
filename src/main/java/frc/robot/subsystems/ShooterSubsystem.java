0d99e6e92b3652d95c2d416953077528286a0a9c
Tyler Melton <ninjanarwhal07@gmail.com>
2024-01-26 11:19:58 -0500

    Added offsets from the elevator pivot

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,13 +9,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-  private double distance = 4.5;
-  private double velocity;
-  private double angle;
-  private double height = 0.7;
+  private static double distance = 2.25;
+  private double distanceOffset;
+  private static double velocity;
+  private static double angle;
+  private static double height;
 
-  private double midpointY = 1.974;
-  private double midpointX = 0.196;
+  private static double midpointY = 1.974;
+  private static double midpointX = 0.196;
+
+  private static double elevatorPivotHeight = Units.inchesToMeters(23.75);
+  private static double elevatorPivotLength = Units.inchesToMeters(12);
+  private static double shooterFromElevator = Units.inchesToMeters(7);
+  private static double pivotAngle = 0;
+  //relative to ground
+  private static double flywheelOffset = Units.degreesToRadians(60); //60 degrees tilted up
+  private static double elevatorXOffset = Units.inchesToMeters(-1);
 
 
   /** Creates a new IntakeSubsystem. */
@@ -28,7 +37,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
   }
 
-  public double findVelocity(double x){
+  public static double findVelocity(double x){
     return Math.sqrt((((
                     (-1*(9.8*Math.pow(-x, 2)))
               /((midpointY + height)/(-x*Math.tan(angle))))
@@ -36,11 +45,11 @@ public class ShooterSubsystem extends SubsystemBase {
                                 /2);
   }
 
-  public double  findTrajectoryPoint(double x){
+  public static double findTrajectoryPoint(double x){
     return((distance + x)*Math.tan(angle)-((9.8*Math.pow((distance + x), 2)) / (2*Math.pow(findVelocity(distance), 2) * Math.pow(Math.cos(angle), 2))));
   }
 
-  public double findBestAngle(){
+  public static double findBestAngle(){
     double add = 0.1;
     int searchLength = 100;
     if(findTrajectoryPoint(-midpointX) < midpointX){
@@ -58,8 +67,17 @@ public class ShooterSubsystem extends SubsystemBase {
     double closestError = 6328;
     int angleIndex = 0;
 
+    double originalDistance = distance;
+
     for(int i = 0; i < searchLength; i++){
       angle = Units.degreesToRadians(angles[i]);
+      height = elevatorPivotHeight-(elevatorPivotLength*Math.cos(angle - flywheelOffset)) + (shooterFromElevator*Math.sin(angle));
+      //System.out.println("\nheight offset: " + height);
+      //System.out.println("Angle: " + Units.radiansToDegrees(angle));
+
+      double xOffset = (elevatorPivotLength*Math.sin(angle - flywheelOffset)) + (shooterFromElevator*Math.sin(Units.degreesToRadians(90) - angle)) + elevatorXOffset;
+      //System.out.println("distance offset: " + xOffset + "\n");
+      distance = originalDistance - xOffset + elevatorXOffset;
       double error = Math.abs(findTrajectoryPoint(-midpointX) - (midpointY - height));
 
       if (error < closestError){
@@ -70,8 +88,22 @@ public class ShooterSubsystem extends SubsystemBase {
     return angles[angleIndex];
   }
 
-  public void calculateShooter(){
+  public static void calculateShooter(){
     angle = findBestAngle();
     velocity = findVelocity(distance);
+    
+    System.out.println("best angle: " + angle);
+    // height = elevatorPivotHeight-(elevatorPivotLength*Math.cos(angle - flywheelOffset)) + (shooterFromElevator*Math.sin(angle));
+    // double originalDistance = distance;
+    // double xOffset = (elevatorPivotLength*Math.sin(angle - flywheelOffset)) + (shooterFromElevator*Math.sin(Units.degreesToRadians(90) - angle)) + elevatorXOffset;
+    // distance = originalDistance - xOffset + elevatorXOffset;
+    // System.out.println("x offset: " + xOffset);
+    // System.out.println("height: " + height);
+    pivotAngle = angle - Units.radiansToDegrees(flywheelOffset);
+    System.out.println("pivot angle: " + pivotAngle);
   }
+
+  // public static void main(String[] args){
+  //   calculateShooter();
+  // }
 }
