8a839bf85293eec26e8cbc8dab8aae4d3103faf1
ethanprivette <dabosses444@gmail.com>
2024-02-06 22:34:22 -0500

    more changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -11,8 +11,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "fd0ec0171e4695dbd45f71e8edbfd13d758db3a3";
   public static final String GIT_DATE = "2024-02-06 19:24:14 EST";
   public static final String GIT_BRANCH = "Mechanism-Sim";
-  public static final String BUILD_DATE = "2024-02-06 21:57:05 EST";
-  public static final long BUILD_UNIX_TIME = 1707274625204L;
+  public static final String BUILD_DATE = "2024-02-06 22:28:40 EST";
+  public static final long BUILD_UNIX_TIME = 1707276520550L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
