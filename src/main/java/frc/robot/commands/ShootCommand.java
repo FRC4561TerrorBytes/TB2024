76bbc886c89457e9d02edd6e6bd0e0d7ea23b639
Tyler Melton <ninjanarwhal07@gmail.com>
2024-02-08 17:44:03 -0500

    Added shoot command and get methods

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -5,35 +5,48 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.drive.Drive;
 
 public class ShootCommand extends Command {
+  /** Creates a new ShootCommand. */
   ShooterSubsystem m_shooterSubsystem;
+  Drive m_driveSubsystem;
+  double targetMPS = 0;
 
-  public ShootCommand(ShooterSubsystem shooterSubsystem) {
+  public ShootCommand(ShooterSubsystem shooterSubsystem, Drive driveSubsystem) {
     m_shooterSubsystem = shooterSubsystem;
+    m_driveSubsystem = driveSubsystem;
     addRequirements(m_shooterSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    m_shooterSubsystem.calculateShooter(m_driveSubsystem.getDistanceFromSpeaker());
+    targetMPS = m_shooterSubsystem.getVelocity();
+    m_shooterSubsystem.setFlywheelSpeed(targetMPS);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    if(m_shooterSubsystem.flywheelUpToSpeed(targetMPS)){
+      m_shooterSubsystem.setIndexerSpeed(Constants.INDEXER_FEED_SPEED);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_shooterSubsystem.stopFlywheel();
+    m_shooterSubsystem.stopIndexer();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_shooterSubsystem.noteShot();
   }
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -18,7 +18,6 @@ import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
-  private final CANSparkMax topShooterMotor;
 
   private double m_velocity;
   private double m_angle;
@@ -138,16 +137,19 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public void calculateShooter(double distance){
-    m_angle = findBestAngle(distance);
+    m_angle = Units.degreesToRadians(findBestAngle(distance));
 
     double originalDistance = distance;
     double xOffset = (Constants.ELEVATOR_PIVOT_LENGTH*Math.sin(m_angle - Constants.FLYWHEEL_OFFSET)) + (Constants.SHOOTER_FROM_ELEVATOR*Math.sin(Units.degreesToRadians(90) - m_angle));
     distance = originalDistance - xOffset + Constants.ELEVATOR_X_OFFSET;
     m_velocity = findVelocity(distance);
 
-    m_angle = Units.degreesToRadians(m_angle);
     m_height = Constants.ELEVATOR_PIVOT_HEIGHT-(Constants.ELEVATOR_PIVOT_LENGTH*Math.cos(m_angle - Constants.FLYWHEEL_OFFSET)) + (Constants.SHOOTER_FROM_ELEVATOR*Math.sin(m_angle));
     m_pivotAngle = m_angle - Units.radiansToDegrees(Constants.FLYWHEEL_OFFSET);
+  }
+
+  public double getVelocity(){
+    return m_velocity;
   }
 
   public double getPivotAngle(){
@@ -163,6 +165,10 @@ public class ShooterSubsystem extends SubsystemBase {
     final MotionMagicVelocityVoltage m_request = new MotionMagicVelocityVoltage(0);
     m_rightFlywheel.setControl(m_request.withVelocity(0));
     m_leftFlywheel.setControl(m_request.withVelocity(0));
+  }
+
+  public boolean flywheelUpToSpeed(double mps){
+    return m_leftFlywheel.getVelocity().getValueAsDouble() >= mps && m_rightFlywheel.getVelocity().getValueAsDouble() >= mps;
   }
 
   public void setIndexerSpeed(double speed){
