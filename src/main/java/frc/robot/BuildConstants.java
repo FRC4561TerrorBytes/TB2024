e22c3d0ede851588917f66aeba02aff44ed2a322
ethanprivette <dabosses444@gmail.com>
2024-02-09 13:13:42 -0500

    forgor to uncomment elevator

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TB2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 67;
-  public static final String GIT_SHA = "96cefb5675f4c852e15e4093f2c560bb15736121";
-  public static final String GIT_DATE = "2024-02-08 12:07:00 EST";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "dddee3a09895940dfeea935fac667bf14306198f";
+  public static final String GIT_DATE = "2024-02-09 13:09:47 EST";
   public static final String GIT_BRANCH = "Mechanism-Sim";
-  public static final String BUILD_DATE = "2024-02-09 12:02:37 EST";
-  public static final long BUILD_UNIX_TIME = 1707498157023L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-02-09 13:12:14 EST";
+  public static final long BUILD_UNIX_TIME = 1707502334176L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
