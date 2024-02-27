699ad78c3339d0e7e0afb233ef5f2874f03ad28b
Vinay <vin07.nag@gmail.com>
2024-02-27 18:50:49 -0500

    Fixed Snap 45 and 90 on drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -223,7 +223,7 @@ public final class Constants {
   public static final int INTAKE_BAR_MOTOR = 17;
   public static final int INTAKE_ROLLER_MOTOR = 18;
 
-  public static final Mode currentMode = Mode.REAL;
+  public static final Mode currentMode = Mode.SIM;
 
   public static enum Mode {
     /** Running on a real robot. */
