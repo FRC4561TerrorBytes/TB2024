37f7aa63611aa3c642e848f7f433cfef70922557
Vinay <vin07.nag@gmail.com>
2024-03-18 17:05:56 -0400

    fixed drivetrain pancake issues

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -70,39 +70,39 @@ public final class Constants {
       new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0,
           -Constants.DRIVETRAIN_WHEELBASE_METERS / 2.0));
    //MODULE 0
-  public static final int FRONT_LEFT_DRIVE_MOTOR = 1; //5 for fulcrum
-  public static final int FRONT_LEFT_STEER_MOTOR = 2; //6 for fulcrum
-  public static final int FRONT_LEFT_STEER_ENCODER = 21; //23 for fulcrum
+  public static final int FRONT_LEFT_DRIVE_MOTOR = 5; //5 for fulcrum
+  public static final int FRONT_LEFT_STEER_MOTOR = 6; //6 for fulcrum
+  public static final int FRONT_LEFT_STEER_ENCODER = 23; //23 for fulcrum
   public static final InvertedValue FRONT_LEFT_DRIVE_MOTOR_INVERTED = InvertedValue.Clockwise_Positive; //counter clockwise for fulcrum
   public static final boolean FRONT_LEFT_TURN_MOTOR_INVERTED = true;
-  public static final double FRONT_LEFT_STEER_OFFSET = Units.degreesToRadians(102.4711046);//0.2393010029 Fulcrum drivebase
+  public static final double FRONT_LEFT_STEER_OFFSET = Units.rotationsToRadians(-0.471680);//0.2393010029 Fulcrum drivebase
 
   //MODULE 1
-  public static final int FRONT_RIGHT_DRIVE_MOTOR = 7; //3 for fulcrum
-  public static final int FRONT_RIGHT_STEER_MOTOR = 8; //4 for fulcrum
-  public static final int FRONT_RIGHT_STEER_ENCODER = 24; //22 for fulcrum
+  public static final int FRONT_RIGHT_DRIVE_MOTOR = 3; //3 for fulcrum
+  public static final int FRONT_RIGHT_STEER_MOTOR = 4; //4 for fulcrum
+  public static final int FRONT_RIGHT_STEER_ENCODER = 22; //22 for fulcrum
   public static final InvertedValue FRONT_RIGHT_DRIVE_MOTOR_INVERTED = InvertedValue.CounterClockwise_Positive;
   public static final boolean FRONT_RIGHT_TURN_MOTOR_INVERTED = true;
-  public static final double FRONT_RIGHT_STEER_OFFSET = Units.degreesToRadians(76);//1.73646625; Fulcrum drivebase
+  public static final double FRONT_RIGHT_STEER_OFFSET = Units.rotationsToRadians(-0.222168);//1.73646625; Fulcrum drivebase
   //MODULE 2
-  public static final int BACK_LEFT_DRIVE_MOTOR = 3; //7 for fulcrum
-  public static final int BACK_LEFT_STEER_MOTOR = 4; //8 for fulcrum
-  public static final int BACK_LEFT_STEER_ENCODER = 22; //24 for fulcum
+  public static final int BACK_LEFT_DRIVE_MOTOR = 7; //7 for fulcrum
+  public static final int BACK_LEFT_STEER_MOTOR = 8; //8 for fulcrum
+  public static final int BACK_LEFT_STEER_ENCODER = 24; //24 for fulcum
   public static final InvertedValue BACK_LEFT_DRIVE_MOTOR_INVERTED = InvertedValue.Clockwise_Positive; //counter clockwise for fulcrum
   public static final boolean BACK_LEFT_TURN_MOTOR_INVERTED = true;
-  public static final double BACK_LEFT_STEER_OFFSET = Units.degreesToRadians(-140);//-2.5632818; Fulcrum drivebase
+  public static final double BACK_LEFT_STEER_OFFSET = Units.rotationsToRadians(0.076660);//-2.5632818; Fulcrum drivebase
 
   //MODULE 3
-  public static final int BACK_RIGHT_DRIVE_MOTOR = 5; //1 for fulcrum
-  public static final int BACK_RIGHT_STEER_MOTOR = 6; //2 for fulcrum
-  public static final int BACK_RIGHT_STEER_ENCODER = 23; //21 for fulcrum
-  public static final InvertedValue BACK_RIGHT_DRIVE_MOTOR_INVERTED = InvertedValue.CounterClockwise_Positive; //clockwise for fulcrum
+  public static final int BACK_RIGHT_DRIVE_MOTOR = 1; //1 for fulcrum
+  public static final int BACK_RIGHT_STEER_MOTOR = 2; //2 for fulcrum
+  public static final int BACK_RIGHT_STEER_ENCODER = 21; //21 for fulcrum
+  public static final InvertedValue BACK_RIGHT_DRIVE_MOTOR_INVERTED = InvertedValue.Clockwise_Positive; //clockwise for fulcrum
   public static final boolean BACK_RIGHT_TURN_MOTOR_INVERTED = true;
-  public static final double BACK_RIGHT_STEER_OFFSET = Units.degreesToRadians(140);//-2.1184274; Fulcrum drivebase
+  public static final double BACK_RIGHT_STEER_OFFSET = Units.rotationsToRadians(-0.331787);//-2.1184274; Fulcrum drivebase
 
   public static final double ARM_CURRENT_LIMIT = 40.0;
 
-  public static final double DRIVE_CURRENT_LIMIT = 50.0;
+  public static final double DRIVE_CURRENT_LIMIT = 40.0;
   public static final int TURN_CURRENT_LIMIT = 20;
   public static final double DRIVE_CURRENT_THRESHOLD = 120.0;
   public static final double DRIVE_CURRENT_TIME_THRESHOLD = 0.1; // seconds
