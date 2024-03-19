5ec91bda5d192558f86ac63d1a85c4684a282d49
tyler <ninjanarwhal07@gmail.com>
2024-03-19 19:23:28 -0400

    Drive PID fixes

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -59,8 +59,8 @@ public class Module {
         turnFeedback = new PIDController(10.0, 0.0, 0.0);
         break;
       default:
-        driveFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
-        driveFeedback = new PIDController(0.0, 0.0, 0.0);
+        driveFeedforward = new SimpleMotorFeedforward(0.25, 2.3);
+        driveFeedback = new PIDController(1, 0.0, 0.0);
         turnFeedback = new PIDController(1.0, 0.0, 0.1);
         break;
     }
