5b9108ec7be5c7aa362b2877dd64922f0fc23b96
ethanprivette <dabosses444@gmail.com>
2024-05-25 15:06:38 -0400

    note align might work or smth

--- src/main/java/frc/robot/commands/AutoNoteAlignCommand.java
+++ src/main/java/frc/robot/commands/AutoNoteAlignCommand.java
@@ -6,11 +6,7 @@ package frc.robot.commands;
 
 import org.littletonrobotics.junction.Logger;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.PathConstraints;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -32,18 +28,11 @@ public class AutoNoteAlignCommand extends Command {
   private final Indexer indexer;
   private final Arm arm;
 
-  private Command pathfindCommand;
+  double camMountHeightIn = 20;
+  double camMountAngleDeg = -22.5;
 
-  double camMountHeightMeters = 20.0;
-  double camMountAngleRad = Units.degreesToRadians(10);
-
-  double camVerticalPOVRad = Units.degreesToRadians(48.9);
-  double camHeightPixels = 480.0;
-  double verticalAnglePerPixel = camVerticalPOVRad / camHeightPixels;
-
-  double camHorizontalPOVRad = Units.degreesToRadians(62.5);
-  double camWidthPixels = 640.0;
-  double horizontalAnglePerPixel = camHorizontalPOVRad / camWidthPixels;
+  double deadzone = 10;
+  double rotDeg = 20;
 
   public AutoNoteAlignCommand(Drive drive, Intake intake, Indexer indexer, Arm arm) {
     this.drive = drive;
@@ -51,7 +40,7 @@ public class AutoNoteAlignCommand extends Command {
     this.indexer = indexer;
     this.arm = arm;
 
-    addRequirements(intake, indexer);
+    addRequirements(intake, indexer, drive);
   }
 
   // Called when the command is initially scheduled.
@@ -67,36 +56,32 @@ public class AutoNoteAlignCommand extends Command {
   @Override
   public void execute() {
     NetworkTable chair = NetworkTableInstance.getDefault().getTable(Constants.DRIVER_LIMELIGHT);
-    NetworkTableEntry txp = chair.getEntry("txp");
-    NetworkTableEntry typ = chair.getEntry("typ");
 
-    double txPixels = txp.getDouble(0.0);
-    double tyPixels = typ.getDouble(0.0);
+    if (chair.getEntry("tv").getDouble(0.0) != 1.0) {return;}
 
-    double yDistanceMeters = -camMountHeightMeters / Math.tan(tyPixels * verticalAnglePerPixel - (camHorizontalPOVRad/2) - camMountAngleRad);
-    double rayToGround = Math.sqrt(Math.pow(camMountHeightMeters, 2) + Math.pow(yDistanceMeters, 2));
-    double xDistanceMeters = rayToGround * Math.tan(txPixels * horizontalAnglePerPixel - (camHorizontalPOVRad / 2));
+    NetworkTableEntry tx = chair.getEntry("tx");
+    double txDeg = tx.getDouble(0.0);
 
-    Logger.recordOutput("Note Align/X Distance", xDistanceMeters);
-    Logger.recordOutput("Note Align/Y Distance", yDistanceMeters);
-
-    Translation2d robotTranslation = drive.getPose().getTranslation();
-    Translation2d noteTranslation = robotTranslation.plus(new Translation2d(xDistanceMeters + 0.25, yDistanceMeters));
-
-    Pose2d notePose = new Pose2d(noteTranslation, drive.getRotation());
-
-    pathfindCommand = AutoBuilder.pathfindToPose(
-      notePose, 
-      new PathConstraints(1.5, 2.0, 540, 540));
-
-    if (chair.getEntry("tv").getDouble(0.0) != 1.0) {
-      return;
+    if (txDeg > deadzone) {
+      drive.runVelocity(new ChassisSpeeds(0, 0, -Units.degreesToRadians(rotDeg)));
+    } else if (txDeg < -deadzone) {
+      drive.runVelocity(new ChassisSpeeds(0, 0, Units.degreesToRadians(rotDeg)));
     }
+
+    NetworkTableEntry ty = chair.getEntry("ty");
+    double tyDeg = ty.getDouble(0.0);
+
+    double angleToGoalRad = Units.degreesToRadians(camMountAngleDeg + tyDeg);
+
+    double distanceIn = (0 - camMountHeightIn) / Math.tan(angleToGoalRad);
+
+    Logger.recordOutput("Note Align/Angle to note", angleToGoalRad);
+    Logger.recordOutput("Note Align/Distance", distanceIn);
 
     intake.setIntakeSpeed(Constants.INTAKE_SPEED);
     indexer.setIndexerSpeed(Constants.INDEXER_FEED_SPEED);
 
-    pathfindCommand.schedule();
+    drive.runVelocity(new ChassisSpeeds(1.5 * (distanceIn / 90), 0, 0));
   }
 
   // Called once the command ends or is interrupted.
@@ -113,6 +98,6 @@ public class AutoNoteAlignCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return pathfindCommand.isFinished();
+    return indexer.noteInIndexer();
   }
 }
